2e66ee8c2bfc88f50f09afb76543a742a8c2c2e8
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-10-31 10:54:29 +0000

    Fix by removing additional quotation marks and ignoring shellcheck

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -10,8 +10,8 @@ DOCKER_IMAGE_TAG="speckle/img2pc"
 if [[ "${CIRCLE_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     IMAGE_VERSION_TAG="${CIRCLE_TAG}"
 else
-    LAST_RELEASE="$(git describe --always --tags "$(git rev-list --tags)" | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)"
-    LAST_RELEASE="${LAST_RELEASE:-"0.0.0"}"
+    #shellcheck disable=SC2046
+    LAST_RELEASE="$(git describe --always --tags $(git rev-list --tags) | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1 || echo "0.0.0")"
     NEXT_RELEASE="$(echo "${LAST_RELEASE}" | python -c "parts = input().split('.'); parts[-1] = str(int(parts[-1])+1); print('.'.join(parts))")"
     BRANCH_NAME_TRUNCATED="$(echo "${CIRCLE_BRANCH}" | cut -c -50 | sed 's/[^a-zA-Z0-9_.-]/_/g')" # docker has a 128 character tag limit, so ensuring the branch name will be short enough
     COMMIT_SHA1_TRUNCATED="$(echo "${CIRCLE_SHA1}" | cut -c -7)"
