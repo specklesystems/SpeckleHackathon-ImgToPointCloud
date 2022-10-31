7aeb48c953642215c20edc0e352e92a8cd0a717c
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-10-31 10:48:32 +0000

    Add LAST_RELEASE to build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -5,13 +5,13 @@ set -eo pipefail
 # defaults to packages for minimal intervention in the ci config
 FOLDER="${FOLDER:-server}"
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 DOCKER_IMAGE_TAG="speckle/img2pc"
 
 if [[ "${CIRCLE_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     IMAGE_VERSION_TAG="${CIRCLE_TAG}"
 else
+    LAST_RELEASE="$(git describe --always --tags "$(git rev-list --tags)" | grep -E '^[0-9]+\.[0-9]+\.[0-9]+$' | head -n 1)"
+    LAST_RELEASE="${LAST_RELEASE:-"0.0.0"}"
     NEXT_RELEASE="$(echo "${LAST_RELEASE}" | python -c "parts = input().split('.'); parts[-1] = str(int(parts[-1])+1); print('.'.join(parts))")"
     BRANCH_NAME_TRUNCATED="$(echo "${CIRCLE_BRANCH}" | cut -c -50 | sed 's/[^a-zA-Z0-9_.-]/_/g')" # docker has a 128 character tag limit, so ensuring the branch name will be short enough
     COMMIT_SHA1_TRUNCATED="$(echo "${CIRCLE_SHA1}" | cut -c -7)"
